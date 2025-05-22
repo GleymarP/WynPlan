718583d724522f048d90f3e522c2799fe8e43d7d
GleymarP <esgleymar@gmail.com>
2025-05-21 22:55:28 -0400

    create json if it doesnt exist

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,34 @@
 #include <QDir>
 #include <QPalette>
 
+void create_json_if_not_exists(const QString& path, const QJsonDocument& default_doc)
+{
+    QFile file(path);
+    if (!file.exists())
+    {
+        QDir().mkpath(QFileInfo(path).absolutePath());
+
+        if (file.open(QIODevice::WriteOnly))
+        {
+            file.write(default_doc.toJson(QJsonDocument::Indented));
+            file.close();
+        }
+    }
+}
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QString path_json = ":/resources/flujograma.json";
-    StudyPlan plan = StudyPlan::load_from_json(path_json);
-
-    QString path_json_teacher = ":/resources/teachers.json";
-    std::vector<Teacher> teachers = Teacher::load_from_json(path_json_teacher, plan);
-
+    QJsonArray default_array;
     QString path_assign_json = QCoreApplication::applicationDirPath() + "/../../resources/assign.json";
-    std::vector<Assigment> assign = Assigment::load_from_json_assing(path_assign_json, plan, teachers);
+    QString path_teachers_json = QCoreApplication::applicationDirPath() + "/../../resources/teachers.json";
+    QString path_plan_json = QCoreApplication::applicationDirPath() + "/../../resources/flujograma.json";
+
+    create_json_if_not_exists(path_assign_json, QJsonDocument(default_array));
+    create_json_if_not_exists(path_teachers_json, QJsonDocument(default_array));
+    create_json_if_not_exists(path_plan_json, QJsonDocument(default_array));
 
 
     QPalette palette;
